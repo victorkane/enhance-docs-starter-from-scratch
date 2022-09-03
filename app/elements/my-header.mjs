4923813afddfce3b51f7ba21326164625a072502
Victor Kane <victorkane@gmail.com>
2022-09-03 12:22:04 -0300

    Initial commit

---
+++ app/elements/my-header.mjs
@@ -0,0 +1,11 @@
+export default function MyHeader({ html }) {
+  return html`
+    <header>
+      <h1>Header</h1>
+      <nav>
+        <a href=/>home</a>
+        <a href=/about>about</a>
+      </nav>
+    </header>
+  `
+}
