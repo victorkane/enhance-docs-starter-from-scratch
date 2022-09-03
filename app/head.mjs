421c197ff8878812760bf13ed0e65ab8a860d22b
Victor Kane <victorkane@gmail.com>
2022-09-03 15:43:19 -0300

    build(head section): override default

---
+++ app/head.mjs
@@ -0,0 +1,13 @@
+export default function Head(req = {}) {
+  const { path } = req
+  const title = `My app — ${path}`
+  return `
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>${title}</title>
+      <link rel="stylesheet" href="/_static/styles.css">
+      <link rel="icon" href="/_static/favicon.svg">
+    </head>
+  `
+}
